32af339806597d9b276743f63a27c05e45127cdb
bastianelm <elmbastian@proton.me>
2024-10-22 10:12:50 +0200

    navigation links

---
+++ nav.js
@@ -0,0 +1,21 @@
+// Die aktuelle URL abrufen
+const currentUrl = window.location.href;
+const url = "https://example.com/path/to/resource";
+const lastSlashIndex = url.lastIndexOf("/");
+const lastPart = url.substring(lastSlashIndex + 1);
+const sideBarLinks = document.querySelectorAll('.sideBarLinks');
+
+function markActiveMenuPoint(){
+    sideBarLinks.forEach(link => {
+        const linkHref = link.getAttribute('href');
+        console.log(linkHref);
+        console.log(lastPart);
+        if (linkHref !== lastPart) {
+            link.classList.remove('activeMenuPoint');
+        } else{
+            link.classList.add('activeMenuPoint');
+        }
+    });
+}
+
+markActiveMenuPoint();
