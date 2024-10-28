7ca092c8a0a252531e6d3423e1b8fb880855fe62
Sven Neuner <neuner.sven@googlemail.com>
2024-10-28 10:13:03 +0100

    update join

--- login.js
+++ login.js
@@ -10,16 +10,40 @@ window.addEventListener('load', () => {
     }, 300); 
 });
 
+    /* Checkbox-Icon's */
+    function toggleCheckBox() {
+        let checkBoxIcons = document.querySelectorAll("#checkBoxIcon");
+    
+        checkBoxIcons.forEach(checkBoxIcon => {
+            if (checkBoxIcon.src.includes("checkbox_icon.svg")) {
+                checkBoxIcon.src = "img/login/Property 1=checked.png";
+            } else {
+                checkBoxIcon.src = "img/login_img/checkbox_icon.svg";
+            }
+            checkBoxIcon.style.width = "24px";
+            checkBoxIcon.style.height = "24px";
+        });
+    }
 
-/* Checkbox */
-function toggleCheckBox() {
-    let checkBoxIcons = document.querySelectorAll("#checkbox-img");
 
-    checkBoxIcons.forEach(checkBoxIcon => {
-        if (checkBoxIcon.src.includes("checkbox_icon.svg")) {
-            checkBoxIcon.src = "img/login/Property 1=Default.png";
+    /*
+     /* Checkbox-Icon's 
+    document.getElementById('loginForm').addEventListener('submit', function(e) {
+        e.preventDefault();  // Standard-Formularabsenden verhindern
+        
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const errorMessage = document.getElementById('error-message');
+    
+        // Beispielbedingung für die Fehlermeldung
+        if (email === "" || password === "") {
+            // Fehlermeldung anzeigen
+            errorMessage.style.display = 'block';
         } else {
-            checkBoxIcon.src = "img/login/Property 1=checked.png";
+            // Fehlermeldung ausblenden, wenn keine Fehler auftreten
+            errorMessage.style.display = 'none';
+            // Weitere Logik zum Senden der Anmeldedaten...
         }
     });
-}
+    
+    */
