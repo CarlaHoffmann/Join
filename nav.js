c5a5d54457a1aa81fcb091e084fb761740f70dd1
Carla Hoffmann <carlahoffmann@gmx.de>
2024-11-22 16:26:56 +0100

    nav bar completed

--- nav.js
+++ nav.js
@@ -1,25 +1,23 @@
-// Die aktuelle URL abrufen
-// const currentUrl = window.location.href;
-// let url = currentUrl;
-// const lastSlashIndex = url.lastIndexOf("/");
-// const lastPart = url.substring(lastSlashIndex + 1);
-// const navigationLinks = document.querySelectorAll('#sideBarNavigation li a, #mobileNav ul li a, .naviLink, #help-link');
-// const content = document.getElementById('content');
-
-// function markActiveMenuPoint(){
-//     navigationLinks.forEach(link => {
-//         link.onclick= function (event){
-//             event.preventDefault();
-//             urlToLoad = link.getAttribute('href');
-//             content.setAttribute('w3-include-html', urlToLoad);
-//             w3.includeHTML();
-//         }
-//     });
-// }
-
-// markActiveMenuPoint();
-
 function toggleHelpMenu() {
     let helpMenu = document.getElementById('help-menu');
     helpMenu.classList.toggle('d-none');
 }
+
+function activeLink() {
+    // Aktuellen Pfad abrufen
+    const currentPath = window.location.pathname.split('/').pop();
+
+    // Alle Links im Sidebar und Mobile Nav durchlaufen
+    const links = document.querySelectorAll('#sidebar a, #mobileNav a');
+
+    links.forEach(link => {
+        // Überprüfen, ob der href des Links mit dem aktuellen Pfad übereinstimmt
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active-link'); // Aktiven Link hinzufügen
+        } else {
+            link.classList.remove('active-link'); // Anderen Links entfernen
+        }
+    });
+}
+
+window.onload = activeLink;
