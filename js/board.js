0f56b20136c9427adf4925de5ffaff1f4f76d47e
Sven Neuner <neuner.sven@googlemail.com>
2024-12-03 20:00:53 +0100

    login update

--- js/board.js
+++ js/board.js
@@ -230,124 +230,161 @@
 //   }
 // }
 
+
+
+const base_url = "https://joinapp-28ae7-default-rtdb.europe-west1.firebasedatabase.app";
+
 async function loadTasks() {
-  try {
-      await loadToDo();
-      await loadInProgress();
-  } catch (error) {
-      console.error("Fehler beim Laden der Tasks:", error);
-  }
+    try {
+        await loadToDo();
+        await loadInProgress();
+        await loadAwaitFeedback();
+        await loadDone();
+    } catch (error) {
+        console.error("Fehler beim Laden der Tasks:", error);
+    }
 }
 
 async function loadToDo() {
-  try {
-      const url = `${base_url}/tasks/toDo.json`;
-      const response = await fetch(url);
+    try {
+        const url = `${base_url}/tasks/toDo.json`;
+        const response = await fetch(url);
 
-      if (!response.ok) {
-          throw new Error(`HTTP-Error: ${response.status}`);
-      }
+        if (!response.ok) {
+            throw new Error(`HTTP-Error: ${response.status}`);
+        }
 
-      const toDo = await response.json();
-      const toDoArray = processTasks(toDo);
-      displayTasks(toDoArray, 'openTasks');
-      console.log(toDoArray);
-  } catch (error) {
-      console.error("Fehler beim Laden der ToDo-Tasks:", error);
-  }
+        const toDo = await response.json();
+        const toDoArray = processTasks(toDo);
+        displayTasks(toDoArray, 'openTasks');
+    } catch (error) {
+        console.error("Fehler beim Laden der ToDo-Tasks:", error);
+    }
 }
 
 async function loadInProgress() {
-  try {
-      const url = `${base_url}/tasks/progress.json`;
-      const response = await fetch(url);
+    try {
+        const url = `${base_url}/tasks/progress.json`;
+        const response = await fetch(url);
 
-      if (!response.ok) {
-          throw new Error(`HTTP-Error: ${response.status}`);
-      }
+        if (!response.ok) {
+            throw new Error(`HTTP-Error: ${response.status}`);
+        }
 
-      const progress = await response.json();
-      const progressArray = processTasks(progress);
-      displayTasks(progressArray, 'inProgressTasks');
-      console.log(progressArray);
-  } catch (error) {
-      console.error("Fehler beim Laden der InProgress-Tasks:", error);
-  }
+        const progress = await response.json();
+        const progressArray = processTasks(progress);
+        displayTasks(progressArray, 'inProgressTasks');
+    } catch (error) {
+        console.error("Fehler beim Laden der InProgress-Tasks:", error);
+    }
+}
+
+async function loadAwaitFeedback() {
+    try {
+        const url = `${base_url}/tasks/feedback.json`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP-Error: ${response.status}`);
+        }
+
+        const feedback = await response.json();
+        const feedbackArray = processTasks(feedback);
+        displayTasks(feedbackArray, 'awaitFeedbackTasks');
+    } catch (error) {
+        console.error("Fehler beim Laden der Feedback-Tasks:", error);
+    }
+}
+
+async function loadDone() {
+    try {
+        const url = `${base_url}/tasks/done.json`;
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP-Error: ${response.status}`);
+        }
+
+        const done = await response.json();
+        const doneArray = processTasks(done);
+        displayTasks(doneArray, 'doneTasks');
+    } catch (error) {
+        console.error("Fehler beim Laden der Done-Tasks:", error);
+    }
 }
 
 function processTasks(tasks) {
-  if (!tasks) return []; // Return an empty array if tasks is null or undefined
+    if (!tasks) return [];
 
-  const taskArray = [];
+    const taskArray = [];
 
-  for (const key in tasks) {
-      if (tasks.hasOwnProperty(key)) {
-          const task = tasks[key];
-          if (!task) continue; // Skip if task is null or undefined
+    for (const key in tasks) {
+        if (tasks.hasOwnProperty(key)) {
+            const task = tasks[key];
+            if (!task) continue;
 
-          const taskObject = {
-              id: key,
-              title: task.title ?? '', // Use nullish coalescing operator for default values
-              category: task.category ?? '',
-              contacts: task.contacts ? Object.values(task.contacts) : [], // Check if contacts is not null before using Object.values
-              date: task.date ?? '',
-              description: task.description ?? '',
-              prio: task.prio ?? 0,
-              subtasks: task.subtasks ? Object.values(task.subtasks) : [], // Check if subtasks is not null before using Object.values
-          };
-          taskArray.push(taskObject);
-      }
-  }
-  return taskArray;
+            const taskObject = {
+                id: key,
+                title: task.title ?? '',
+                category: task.category ?? '',
+                contacts: task.contacts ? Object.values(task.contacts) : [],
+                date: task.date ?? '',
+                description: task.description ?? '',
+                prio: task.prio ?? 0,
+                subtasks: task.subtasks ? Object.values(task.subtasks) : [],
+            };
+            taskArray.push(taskObject);
+        }
+    }
+    return taskArray;
 }
 
 function displayTasks(taskArray, containerId) {
-  let tasks = document.getElementById(containerId);
-  let taskHTML = '';
+    const tasks = document.getElementById(containerId);
+    let taskHTML = '';
 
-  taskArray.forEach(task => {
-      let subtasksText = `${task.subtasks.length} von ${task.subtasks.length} Subtasks`;
-      let contactsHTML = task.contacts.map(contact => `<div class="member" style="background-color: #7f5af0;">${contact}</div>`).join('');
-      let prio = getPrio(task.prio);
-      console.log(prio);
-      taskHTML += `
-          <div class="task-card">
-              <div class="task-type">${task.category}</div>
-              <h3 class="task-title">${task.title}</h3>
-              <p class="task-description">${task.description}</p>
-              <div class="progress-section">
-                  <div class="progress">
-                      <div class="progress-bar" style="width: 50%;"></div>
-                  </div>
-                  <p class="subtasks">${subtasksText}</p>
-              </div>
-              <div class="members-section">
-                  <div class="members">
-                      ${contactsHTML}
-                      <div class="member" style="background-color: #ff85a1;">DE</div>
-                      <div class="member" style="background-color: #ffd803;">EF</div>
-                  </div>
-                  <div class="priority">
-                      <img src="./assets/img/add_task/prio_${prio}.svg" alt="medium icon">
-                  </div>
-              </div>
-          </div>
-      `;
-  });
+    taskArray.forEach(task => {
+        const subtasksText = `${task.subtasks.length} von ${task.subtasks.length} Subtasks`;
+        const contactsHTML = task.contacts.map(contact => `<div class="member" style="background-color: #7f5af0;">${contact}</div>`).join('');
+        const prio = getPrio(task.prio);
 
-  tasks.innerHTML = taskHTML;
+        taskHTML += `
+            <div id="task-${task.id}" class="task-card" draggable="true" ondragstart="drag(event)">
+                <div class="task-type">${task.category}</div>
+                <h3 class="task-title">${task.title}</h3>
+                <p class="task-description">${task.description}</p>
+                <div class="progress-section">
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 50%;"></div>
+                    </div>
+                    <p class="subtasks">${subtasksText}</p>
+                </div>
+                <div class="members-section">
+                    <div class="members">${contactsHTML}</div>
+                    <div class="priority">
+                        <img src="./assets/img/add_task/prio_${prio}.svg" alt="${prio} icon">
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+
+    tasks.innerHTML = taskHTML;
 }
 
 function getPrio(priority) {
-  console.log(priority);
-  switch (priority) {
-      case '1':
-          return 'urgent';
-      case '2':
-          return 'medium';
-      case '3':
-          return 'low';
-      // default:
-      //     return 'medium';
-  }
+    switch (priority) {
+        case '1':
+            return 'urgent';
+        case '2':
+            return 'medium';
+        case '3':
+            return 'low';
+        default:
+            return 'medium';
+    }
 }
+
+
+
+
