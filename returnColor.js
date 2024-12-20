e1e5b9e4b6f7659e62795119b9dea818afb783df
Carla Hoffmann <carlahoffmann@gmx.de>
2024-12-20 10:45:38 +0100

    added return color

--- returnColor.js
+++ returnColor.js
@@ -1,11 +1,11 @@
 function returnColor(){
     const rgbaColorArrays = [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0],
+        [255,122,0],
+        [147,39,255],
+        [110, 82, 255],
+        [252, 113, 255],
+        [255, 187, 43],
+        [31,215,193]
     ];
     const random = Math.floor(Math.random() * ((rgbaColorArrays.length-1) - 0 + 1)) + 0;
     const randomColor = 'rgba(' + rgbaColorArrays[random] + ')';
