75f8b23439fc9125653167426f71a878d080dba2
Carla Hoffmann <carlahoffmann@gmx.de>
2024-12-23 19:01:17 +0100

    added docu

--- returnColor.js
+++ returnColor.js
@@ -4,14 +4,16 @@
  */
 function returnColor(){
     const rgbaColorArrays = [
-        [255,122,0],
-        [147,39,255],
-        [110, 82, 255],
-        [252, 113, 255],
-        [255, 187, 43],
-        [31,215,193]
+        [255,122,0],    /**Orange */
+        [147,39,255],   /**Purple */
+        [110, 82, 255], /**Light Blue */
+        [252, 113, 255],/**Pink */
+        [255, 187, 43], /**Yellow */
+        [31,215,193]    /**Teal */
     ];
+    /**Generate a random index to select a color from the array */
     const random = Math.floor(Math.random() * ((rgbaColorArrays.length-1) - 0 + 1)) + 0;
+    /**Construct the RGBA color string */
     const randomColor = 'rgba(' + rgbaColorArrays[random] + ')';
     return randomColor; 
 }
