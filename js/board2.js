561398d7d810336decb90b9cdb3c6f916f780714
CarlaHoffmann <carlahoffmann@gmx.de>
2025-01-21 16:11:43 +0100

    added board js

--- js/board2.js
+++ js/board2.js
@@ -249,7 +249,7 @@ async function deleteTask(taskId) {
 
             taskElement.remove();
 
-            updatePlaceholders();
+            // updatePlaceholders();
 
             currentTask = [];
             closeTaskOverlay();
@@ -269,18 +269,25 @@ function updatePlaceholders() {
     columns.forEach(column => {
         const tasksContainer = document.getElementById(column + "Tasks");
         const placeholder = document.getElementById(column + "Placeholder");
-        if (tasksContainer.contains('task-card')) {
+        console.log(tasksContainer, placeholder);
+        
+        if (!tasksContainer || !placeholder) {
+            console.error(`Container or placeholder for ${column} not found`);
+            return; // Skip this iteration if elements are not found
+        }
+
+        if (tasksContainer.querySelector('.task-card')) {
             // placeholder.style.display = "block";
-            if(placeholder.contains('hide')) {
-                placeholder.classList.remove('hide');
-            }
-            placeholder.classList.add('show');
+            // if(placeholder.contains('hide')) {
+                placeholder.classList.remove('show');
+            // }
+            placeholder.classList.add('hide');
         } else {
             // placeholder.style.display = "none";
-            if(placeholder.contains('show')) {
-                placeholder.classList.remove('show');
-            }
-            placeholder.classList.add('hide');
+            // if(placeholder.contains('show')) {
+                placeholder.classList.remove('hide');
+            // }
+            placeholder.classList.add('show');
         }
     });
 }
